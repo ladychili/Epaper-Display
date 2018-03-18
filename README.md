b8f955048117288cd32af33f93c817d59a0ae611
LadyChili <27876613+ladychili@users.noreply.github.com>
2018-03-18 13:23:17 +0000

    final modification b4 poster presentation

--- README.md
+++ README.md
@@ -1,26 +1,47 @@
 ## Epaper-Display
 
-#### 1.1 
-- weather string fixed
-- countdown fomula completed
-- change waiting time from 20sec into 30sec
-- change countdown feature into twitter direct message
-- two new modules imported: tweepy & textwrap
+
+#### v1.3
+- Countdown function added back.
+- Time digit font Updated. (New bitmap files added)
+- Adapt the layout of date section.
+
+##### Diagram
+
+<img src="diagram.png" width="600" height="260" />
+
+##### Layout
+
+<img src="EPD_layout.png" width="480" height="320" />
+
+17 March 2018
+
+#### v1.2
+- Quotes-section added.
+- Left-shifted right-hand section by 10 px.
+- Fixed weather string bug(List index out of range when *weather* only contains 1 word e.g.'Clear').
+- Altered encoding standard from *GB2312* to *GB18030*. 
+- Altered the number of twitter statuses fetched each time, from 10 to 15.
+
+15 March 2018
+
+
+#### v1.1 
+- Weather string truncated.
+- Countdown fomula completed.
+- Altered waiting time from 20sec into 30sec.
+- Replaced countdown feature into twitter direct message.
+- Two new modules imported: tweepy & textwrap.
 
 11 March 2018
 
 
-#### 1.0 Beta 
+#### v1.0 Beta 
 
-weather: length unfixed
-
-Time: BMP file Dimensions unfixed
-
-Film section: TBA
-
-Book section: TBA
-
-Countdown fomula: TBA
-
+- weather: length unfixed
+- Time: BMP file Dimensions unfixed
+- Film section: TBA
+- Book section: TBA
+- Countdown fomula: TBA
 
 3 March 2018
--- main.py
+++ main.py
@@ -13,10 +13,13 @@ class DisplayAll(threading.Thread):
     def __init__(self):  
         threading.Thread.__init__(self)
         self.wdict = None
+        self.filmlines = None
+        self.booklines = None
         # self.init = True
         # self.load_data()
         self.init_screen()
         self.fetch_weather()
+        self.fetch_quotes()
 
     def init_screen(self):
         self.screen = Screen('/dev/ttyAMA0')
@@ -28,8 +31,11 @@ class DisplayAll(threading.Thread):
     def run(self):
         while True:
             minute = time.strftime("%M") 
-            if minute == '01' or minute == '31':
+            hour = time.strftime("%H")
+            if minute == '01' or '31': # update weather every 30m
                 self.fetch_weather()
+            if hour == '06' or '18': # update quote every 12h
+                self.fetch_quotes()
             self.update_screen()
             time.sleep(60)
 
@@ -38,30 +44,41 @@ class DisplayAll(threading.Thread):
         self.show_timedate()
         self.show_weather()
         self.show_indoor_cond()
-        self.twitter_DM()
+        self.show_cntdown()
         self.book_movie()
         self.screen.update()
 
     
     def show_timedate(self):
-        clock_x = 40
+        clock_x = 5
         clock_y = 40
         temp_x = 0
         time_now = datetime.datetime.now()
         time_str = time_now.strftime('%H:%M')
-        date_str = time_now.strftime('%Y-%m-%d')
-        week_str = time_now.strftime('%A')
+        date_str = time_now.strftime('%d/%m/%Y')
+        week_str = time_now.strftime('%a')
     
         self.screen.set_en_font_size(FONT_SIZE_48)
         for c in time_str:
-            w_bmp = 'NUM{}.BMP'.format('S' if c == ':' else c)
-            self.screen.bitmap(clock_x + temp_x, clock_y, w_bmp)
-            temp_x += 70 if c == ':' else 100
-        self.screen.text(clock_x + 350 + 140, clock_y + 10, date_str)
-        self.screen.text(clock_x + 350 + 170, clock_y + 70, week_str)
+            # n_bmp = 'NUM{}.BMP'.format('S' if c == ':' else c)
+            n_bmp = 'N{}.BMP'.format('UMS' if c == ':' else c)
+            self.screen.bitmap(clock_x + temp_x, clock_y, n_bmp)
+            temp_x += 60 if c == ':' else 91
+        self.screen.text(clock_x + 450 , clock_y + 90, date_str)
+        self.screen.text(clock_x + 450 + 250, clock_y + 90, week_str)
         self.screen.line(0, clock_y + 160, 800, clock_y + 160)
         self.screen.line(0, clock_y + 161, 800, clock_y + 161)
-        self.screen.line(240, 200, 240, 600)
+        self.screen.line(230, 200, 230, 600)
+        self.screen.line(230, 360, 800, 360)
+
+        TheDay = datetime.date(2018,04,11)
+        Today  = datetime.date.today()
+        cntdown = (TheDay - Today).days
+        self.screen.set_en_font_size(FONT_SIZE_64)
+        self.screen.text(clock_x + 450 + 10, clock_y, str(cntdown))
+        self.screen.set_en_font_size(FONT_SIZE_32)
+        self.screen.text(clock_x + 450 + 10 + 68, clock_y+25, 'days till in-class test')
+        self.screen.set_en_font_size(FONT_SIZE_48)
     
     
     def fetch_weather(self):
@@ -69,16 +86,33 @@ class DisplayAll(threading.Thread):
         json_str = json_web.read()
         self.wdict    = json.loads(json_str)  
 
+    def fetch_quotes(self):
+        statuses = api.user_timeline(id='MovieQuotesPage',count=15)
+        for s in statuses:
+            if len(s.text) <= 80:
+                tweet = s.text
+        self.filmlines = textwrap.wrap(tweet,39)
+
+        bookstatuses = api.user_timeline(id='HereBestBooks',count=15)
+        for s in bookstatuses:
+            if len(s.text) <= 80:
+                booktweet = s.text
+        self.booklines = textwrap.wrap(booktweet,39)
+
+
+
     def show_weather(self):
 
-        self.screen.text(20, 220,'Cambridge')  # Location
-        
-        
+        self.screen.text(15, 220,'Cambridge')  # Location
         temp = self.wdict['current_observation']['temp_c']
         temp = str(temp)
         humi = self.wdict['current_observation']['relative_humidity']
         cw_f = self.wdict['current_observation']['weather']
-        cw   = cw_f.split()[1]
+        cw_f  = cw_f.split()
+        if len(cw_f) > 1:
+            cw = cw_f[1]
+        else:
+            cw = cw_f[0]
         self.screen.text(40, 460, cw)
         self.screen.text(1, 530, temp + '℃')
         self.screen.text(140, 530, humi)
@@ -108,37 +142,34 @@ class DisplayAll(threading.Thread):
         h, t = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
         rtemp = str(int(t)) + '℃'
         rhumi = str(int(h)) + ' %'
-        self.screen.bitmap(260, 250, 'TEMP.BMP')
-        self.screen.bitmap(260, 300, 'HUMI.BMP') 
-        self.screen.text(300,240, rtemp)
-        self.screen.text(300,290, rhumi)
+        self.screen.bitmap(250, 250, 'TEMP.BMP')
+        self.screen.bitmap(250, 300, 'HUMI.BMP') 
+        self.screen.text(290,240, rtemp)
+        self.screen.text(290,290, rhumi)
     
     
-    def twitter_DM(self):
+    def show_cntdown(self):
         DM = api.direct_messages(count=1,full_text=True) 
         msg = DM[0].text
         wrap = textwrap.wrap(msg,26)
-        self.screen.bitmap(420, 250, 'MSG.BMP')
+        self.screen.bitmap(410, 250, 'MSG.BMP')
         self.screen.set_en_font_size(FONT_SIZE_32)
-        self.screen.text(500,250, wrap[0])
+        self.screen.text(490,250, wrap[0])
         if len(wrap) > 1:
-            self.screen.text(500,290, wrap[1])
+            self.screen.text(490,290, wrap[1])
 
-        '''
-        TheDay = datetime.date(2018,06,22)
-        Today  = datetime.date.today()
-        cntdown = (TheDay - Today).days
-        self.screen.bitmap(420, 250, 'MSG.BMP')
-        self.screen.set_en_font_size(FONT_SIZE_32)
-        self.screen.text(500,250, 'There are               days')
-        self.screen.text(500,290, 'until going home!')
-        self.screen.set_en_font_size(FONT_SIZE_64)
-        self.screen.text(650,220, str(cntdown))
-        '''
     
     def book_movie(self):
-        self.screen.bitmap(250, 350, 'FILM.BMP')
-        self.screen.bitmap(250, 450, 'BOOK.BMP')
+        self.screen.bitmap(240, 395, 'FILM.BMP')
+        self.screen.bitmap(240, 505, 'BOOK.BMP')
+
+        self.screen.text(320,390, self.filmlines[0])
+        if len(self.filmlines) > 1:
+            self.screen.text(320,430, self.filmlines[1])
+        self.screen.text(320,500, self.booklines[0])
+        if len(self.booklines) > 1:
+            self.screen.text(320,540, self.booklines[1])
+
 
 import textwrap
 import tweepy
